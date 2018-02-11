dc42dfa3b76c1ce95e699819057c502b8f7b6d9f
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-11 20:50:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function endGame() {
 
   ROCKS.forEach(function(rock) {rock.remove()}); //clears rocks from the screen 
 
-  document.removeEventListener('keydown', moveDodger()); //stop listening to arrow inputs
+  document.removeEventListener('keydown', moveDodger; //stop listening to arrow inputs
 
   START.innerHTML = 'PLAY AGAIN?';
   START.style.display = 'inline';
